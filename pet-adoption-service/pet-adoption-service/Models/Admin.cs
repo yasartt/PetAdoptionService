72b3e2c3ff1dc9a6c1a3d3a1c009862d118d62ce
Elif Sorguç <elif.sorguc@ug.bilkent.edu.tr>
2023-12-21 15:59:56 +0300

    admin model

---
+++ pet-adoption-service/pet-adoption-service/Models/Admin.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using pet_adoption_service.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace pet_adoption_service.Models
+{
+    public class Admin
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("user_id")]
+        public int UserId { get; set; }
+        [Required]
+        [Column("username")]
+        [MaxLength(255)]
+        public string Username { get; set; }
+
+        [Required]
+        [Column("password")]
+        [MaxLength(255)]
+        public string Password { get; set; }
+    }
+}
